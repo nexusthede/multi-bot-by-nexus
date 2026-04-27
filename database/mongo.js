83a3006272ee8b727d25e6213fa6b169d6e04384
nexusthede <ivanrrr76@gmail.com>
2026-04-27 15:26:06 -0700

    Update mongo.js

--- database/mongo.js
+++ database/mongo.js
@@ -1,1 +1,16 @@
+const mongoose = require("mongoose");
 
+module.exports = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      console.log("⚠️ Mongo URI not found in environment variables");
+      return;
+    }
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+  }
+};
