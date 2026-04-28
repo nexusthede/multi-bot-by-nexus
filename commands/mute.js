57b21e1d71c5f58cf3581929b4fa3dc3033791e6
nexusthede <ivanrrr76@gmail.com>
2026-04-28 00:59:00 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -1,15 +1,25 @@
-const { EmbedBuilder } = require("discord.js");
+const { actionEmbed, failEmbed, permissionEmbed } = require("../utils/embeds");
+const { PermissionsBitField } = require("discord.js");
 
 module.exports = {
   name: "mute",
+  aliases: ["timeout", "m"],
+
   async execute(message, args) {
-    const t = message.mentions.members.first();
+    const target = message.mentions.members.first();
     const time = args[1];
-    if (!t || !time) return message.reply("user + time");
 
-    const ms = time.endsWith("m") ? parseInt(time)*60000 : parseInt(time)*3600000;
-    await t.timeout(ms).catch(()=>{});
+    if (!target || !time)
+      return message.reply({ embeds: [failEmbed("Usage: .mute @user time")] });
 
-    message.reply({ embeds:[new EmbedBuilder().setTitle("MUTED").setColor(0x2ECC71).setDescription(t.user.tag)] });
+    const ms = time.endsWith("m")
+      ? parseInt(time) * 60000
+      : parseInt(time) * 3600000;
+
+    await target.timeout(ms);
+
+    return message.reply({
+      embeds: [actionEmbed("mute", target, message.author)]
+    });
   }
 };
