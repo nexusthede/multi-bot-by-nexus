086cfccbfafec95c4c30c166adf858e8521da2e0
nexusthede <ivanrrr76@gmail.com>
2026-04-28 01:02:54 -0700

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,1 +1,19 @@
+const { purgeEmbed, failEmbed, permissionEmbed } = require("../utils/embeds");
+const { PermissionsBitField } = require("discord.js");
 
+module.exports = {
+  name: "purge",
+  aliases: ["clear", "prune"],
+
+  async execute(message, args) {
+    const amount = parseInt(args[0]);
+    if (!amount)
+      return message.reply({ embeds: [failEmbed("Provide amount")] });
+
+    await message.channel.bulkDelete(amount, true);
+
+    return message.reply({
+      embeds: [purgeEmbed(amount, message.author, message.channel)]
+    });
+  }
+};
