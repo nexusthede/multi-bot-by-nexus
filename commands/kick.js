a6c0779d6bb1656725179fecdcca9a83af861e0c
nexusthede <ivanrrr76@gmail.com>
2026-04-28 00:58:41 -0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,11 +1,22 @@
-const { EmbedBuilder } = require("discord.js");
+const { actionEmbed, failEmbed, permissionEmbed } = require("../utils/embeds");
+const { PermissionsBitField } = require("discord.js");
 
 module.exports = {
   name: "kick",
+  aliases: ["k"],
+
   async execute(message, args) {
-    const t = message.mentions.members.first();
-    if (!t) return message.reply("Mention user");
-    await t.kick().catch(()=>{});
-    message.reply({ embeds:[new EmbedBuilder().setTitle("KICKED").setColor(0x2ECC71).setDescription(t.user.tag)] });
+    const target = message.mentions.members.first();
+    if (!target)
+      return message.reply({ embeds: [failEmbed("Mention a user")] });
+
+    if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers))
+      return message.reply({ embeds: [permissionEmbed("Kick Members")] });
+
+    await target.kick();
+
+    return message.reply({
+      embeds: [actionEmbed("kick", target, message.author)]
+    });
   }
 };
