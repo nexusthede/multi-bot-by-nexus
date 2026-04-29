53984e966612d07e94c25af4c933dd7f25f6b32c
nexusthede <ivanrrr76@gmail.com>
2026-04-29 13:49:00 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -32,13 +32,13 @@ module.exports = {
         : 10 * 60 * 1000;
 
     if (!target)
-      return message.reply({ embeds: [fail("No user mentioned")] });
+      return message.reply({ embeds: [fail("> No user mentioned")] });
 
     if (!hasAccess(message.member, access.mod))
-      return message.reply({ embeds: [permission("Moderate Members")] });
+      return message.reply({ embeds: [permission("> Moderate Members")] });
 
     if (isProtected(target))
-      return message.reply({ embeds: [fail("This user is protected")] });
+      return message.reply({ embeds: [fail("> This user is protected")] });
 
     const check = checkHierarchy(message, target);
 
@@ -52,7 +52,7 @@ module.exports = {
 
     return message.reply({
       embeds: [
-        success(`${target.user.tag} was muted by ${message.author.tag}`)
+        success(`> <@${target.id}> was muted by <@${message.author.id}>`)
       ]
     });
   }
