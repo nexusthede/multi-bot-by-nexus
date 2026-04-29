0f903ff528db5a8374394abe5bbee10f026aa396
nexusthede <ivanrrr76@gmail.com>
2026-04-28 21:31:37 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -16,41 +16,29 @@ const {
 
 module.exports = {
   name: "mute",
+  aliases: ["timeout", "tm"],
 
-  async execute(message, args) {
+  async execute(message) {
     const target = message.mentions.members.first();
 
     if (!target)
-      return message.reply({
-        embeds: [fail("No user mentioned")]
-      });
+      return message.reply({ embeds: [fail("No user mentioned")] });
 
     if (!hasAccess(message.member, access.mod))
-      return message.reply({
-        embeds: [permission("Moderate Members")]
-      });
+      return message.reply({ embeds: [permission("Moderate Members")] });
 
     if (isProtected(target))
-      return message.reply({
-        embeds: [fail("This user is protected")]
-      });
+      return message.reply({ embeds: [fail("This user is protected")] });
 
     const check = checkHierarchy(message, target);
 
     if (check === "USER")
-      return message.reply({
-        embeds: [hierarchyUser(target)]
-      });
+      return message.reply({ embeds: [hierarchyUser(target)] });
 
     if (check === "BOT")
-      return message.reply({
-        embeds: [hierarchyBot(target)]
-      });
+      return message.reply({ embeds: [hierarchyBot(target)] });
 
-    // default mute time (10 minutes)
-    const duration = 10 * 60 * 1000;
-
-    await target.timeout(duration);
+    await target.timeout(10 * 60 * 1000);
 
     return message.reply({
       embeds: [
