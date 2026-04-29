a4911f6929a5928f2d295e7a039ca43da1af1548
nexusthede <ivanrrr76@gmail.com>
2026-04-28 21:46:38 -0700

    Update unjail.js

--- commands/unjail.js
+++ commands/unjail.js
@@ -1,1 +1,52 @@
+const {
+  fail,
+  permission,
+  log
+} = require("../utils/embeds/embedjail");
 
+const access = require("../config/access");
+
+module.exports = {
+  name: "unjail",
+  aliases: ["uj"],
+
+  async execute(message) {
+    if (!message.guild || message.author.bot) return;
+
+    const target = message.mentions.members.first();
+
+    const jailRole = message.guild.roles.cache.get("1492118672267939861");
+    const logChannel = message.guild.channels.cache.get("1497815554956853359");
+
+    // ❌ no user
+    if (!target)
+      return message.reply({ embeds: [fail("No user mentioned")] });
+
+    // ⚖ admin only
+    if (!message.member.roles.cache.some(r => access.admin.includes(r.id)))
+      return message.reply({ embeds: [permission()] });
+
+    // ❌ not jailed
+    if (!target.roles.cache.has(jailRole.id))
+      return message.reply({ embeds: [fail("User is not jailed")] });
+
+    // 🔓 unjail
+    await target.roles.remove(jailRole).catch(() => {
+      return message.reply({ embeds: [fail("Failed to unjail user")] });
+    });
+
+    // 📜 logs
+    if (logChannel) {
+      logChannel.send({
+        embeds: [
+          log(target.user.tag, message.author.tag)
+            .setDescription(
+              `**JAIL EVENT**\n• User\n> ${target.user.tag}\n• Action\n> unjailed\n• By\n> ${message.author.tag}`
+            )
+        ]
+      }).catch(() => {});
+    }
+
+    return message.reply(`> ${target.user.tag} has been unjailed.`);
+  }
+};
