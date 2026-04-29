31b83090cb436a930cf43c4247a8bed48c5fa53d
nexusthede <ivanrrr76@gmail.com>
2026-04-28 21:27:02 -0700

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,19 +1,40 @@
-const { purgeEmbed, failEmbed, permissionEmbed } = require("../utils/embeds");
-const { PermissionsBitField } = require("discord.js");
+const access = require("../config/access");
+
+const {
+  fail,
+  permission,
+  success
+} = require("../utils/embeds/embedmod");
+
+const {
+  hasAccess
+} = require("../utils/guards");
 
 module.exports = {
   name: "purge",
-  aliases: ["clear", "prune"],
 
   async execute(message, args) {
     const amount = parseInt(args[0]);
-    if (!amount)
-      return message.reply({ embeds: [failEmbed("Provide amount")] });
 
+    // no number
+    if (!amount || amount <= 0 || amount > 100)
+      return message.reply({
+        embeds: [fail("Provide a number between 1 and 100")]
+      });
+
+    // admin only
+    if (!hasAccess(message.member, access.admin))
+      return message.reply({
+        embeds: [permission("Administrator")]
+      });
+
+    // delete messages
     await message.channel.bulkDelete(amount, true);
 
     return message.reply({
-      embeds: [purgeEmbed(amount, message.author, message.channel)]
+      embeds: [
+        success(`Deleted ${amount} messages`)
+      ]
     });
   }
 };
