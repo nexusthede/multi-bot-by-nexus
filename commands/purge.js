3fe160d0caba2f9a3f3beef90eef2319af55d71b
nexusthede <ivanrrr76@gmail.com>
2026-04-28 21:32:32 -0700

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -6,35 +6,29 @@ const {
   success
 } = require("../utils/embeds/embedmod");
 
-const {
-  hasAccess
-} = require("../utils/guards");
+const { hasAccess } = require("../utils/guards");
 
 module.exports = {
   name: "purge",
+  aliases: ["clear", "clean"],
 
   async execute(message, args) {
     const amount = parseInt(args[0]);
 
-    // no number
-    if (!amount || amount <= 0 || amount > 100)
+    if (!amount || amount < 1 || amount > 100)
       return message.reply({
-        embeds: [fail("Provide a number between 1 and 100")]
+        embeds: [fail("Provide a number between 1-100")]
       });
 
-    // admin only
     if (!hasAccess(message.member, access.admin))
       return message.reply({
         embeds: [permission("Administrator")]
       });
 
-    // delete messages
     await message.channel.bulkDelete(amount, true);
 
     return message.reply({
-      embeds: [
-        success(`Deleted ${amount} messages`)
-      ]
+      embeds: [success(`Deleted ${amount} messages`)]
     });
   }
 };
