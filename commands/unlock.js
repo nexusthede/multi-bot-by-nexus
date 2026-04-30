bbcc48a7d9bcc06a01659ccacc5efaca6ab430ec
nexusthede <ivanrrr76@gmail.com>
2026-04-30 12:05:31 -0700

    Update unlock.js

--- commands/unlock.js
+++ commands/unlock.js
@@ -1,6 +1,6 @@
-const access = require("../config/access");
-const { unlock, fail } = require("../utils/embeds/embedchannels");
+const { unlock, fail, permission } = require("../utils/embeds/embedchannels");
 const { PermissionsBitField } = require("discord.js");
+const { canUse } = require("../utils/perms");
 
 module.exports = {
   name: "unlock",
@@ -9,12 +9,16 @@ module.exports = {
   async execute(message) {
     if (!message.guild || message.author.bot) return;
 
-    const isOwner = access.owner?.some(id => message.member.roles.cache.has(id));
-    const isAdmin = access.admin?.some(id => message.member.roles.cache.has(id));
+    // 🔐 CENTRAL PERMISSION CHECK
+    if (!canUse(message.member, "lock"))
+      return message.channel.send({
+        embeds: [permission("Owner / Admin Access Required")]
+      });
 
-    if (!isOwner && !isAdmin) return;
-
-    if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.ManageChannels)) {
+    // ⚠ bot permission check
+    if (!message.guild.members.me.permissions.has(
+      PermissionsBitField.Flags.ManageChannels
+    )) {
       return message.channel.send({
         embeds: [fail("Bot missing Manage Channels permission")]
       });
@@ -22,10 +26,16 @@ module.exports = {
 
     const channel = message.channel;
 
-    await channel.permissionOverwrites.edit(
-      message.guild.roles.everyone,
-      { SendMessages: true }
-    ).catch(() => {});
+    try {
+      await channel.permissionOverwrites.edit(
+        message.guild.roles.everyone,
+        { SendMessages: true }
+      );
+    } catch (err) {
+      return message.channel.send({
+        embeds: [fail("Failed to unlock channel")]
+      });
+    }
 
     return message.channel.send({
       embeds: [unlock(channel.id)]
