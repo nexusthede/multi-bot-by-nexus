da036e595ea91535826a400a24a6b1e7b1faf74b
nexusthede <ivanrrr76@gmail.com>
2026-04-30 00:06:22 -0700

    Create role.js

---
+++ commands/role.js
@@ -0,0 +1,46 @@
+module.exports = {
+  name: "role",
+  aliases: ["r"],
+
+  async execute(message) {
+    const target = message.mentions.members.first();
+    const role = message.mentions.roles.first();
+
+    if (!target || !role)
+      return message.channel.send({
+        embeds: [fail("Usage: .role @user @role")]
+      });
+
+    if (
+      !hasAccess(message.member, access.owner) &&
+      !hasAccess(message.member, access.admin) &&
+      !hasAccess(message.member, access.srmod)
+    )
+      return message.channel.send({
+        embeds: [permission("Owner / Admin / Sr Mod Only")]
+      });
+
+    if (isProtected(target))
+      return message.channel.send({
+        embeds: [fail("This user is protected")]
+      });
+
+    const check = checkHierarchy(message, target);
+
+    if (check === "USER")
+      return message.channel.send({
+        embeds: [fail("User has higher role hierarchy")]
+      });
+
+    if (check === "BOT")
+      return message.channel.send({
+        embeds: [fail("Bot role is too low")]
+      });
+
+    await target.roles.add(role);
+
+    return message.channel.send({
+      embeds: [roleAdded(target, role)]
+    });
+  }
+};
