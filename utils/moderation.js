1d696dd05e776872aa6af6292dfa423badf6961f
nexusthede <ivanrrr76@gmail.com>
2026-04-30 02:05:50 -0700

    Update moderation.js

--- utils/moderation.js
+++ utils/moderation.js
@@ -1,1 +1,68 @@
+const access = require("../config/access");
+const { hasAccess } = require("./guards");
 
+/**
+ * Helper: build staff list quickly
+ */
+function staffList(extra = []) {
+  return [
+    ...access.owner,
+    ...access.admin,
+    ...access.srmod,
+    ...access.mod,
+    ...access.trialmod,
+    ...access.helper,
+    ...extra
+  ];
+}
+
+/**
+ * BAN PERMISSION
+ * Owner + Admin + SrMod only (tighter control)
+ */
+function canBan(member) {
+  return hasAccess(member, [
+    ...access.owner,
+    ...access.admin,
+    ...access.srmod
+  ]);
+}
+
+/**
+ * KICK PERMISSION
+ * Slightly wider than ban
+ */
+function canKick(member) {
+  return hasAccess(member, staffList([
+    ...access.admin,
+    ...access.srmod,
+    ...access.mod
+  ]));
+}
+
+/**
+ * WARN PERMISSION
+ * All staff can warn
+ */
+function canWarn(member) {
+  return hasAccess(member, staffList());
+}
+
+/**
+ * PURGE / CLEAR PERMISSION
+ * Mods and above
+ */
+function canPurge(member) {
+  return hasAccess(member, staffList([
+    ...access.admin,
+    ...access.srmod,
+    ...access.mod
+  ]));
+}
+
+module.exports = {
+  canBan,
+  canKick,
+  canWarn,
+  canPurge
+};
